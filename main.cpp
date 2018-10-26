a76c76d9136d13127d8d2a2cbaeb543897305e6d
Sam Adlin <sadlin53@gmail.com>
2018-10-26 10:21:23 -0400

    Changed from using for loops to while and do-while loops

--- main.cpp
+++ main.cpp
@@ -5,58 +5,70 @@
 using namespace std;
 int main() {
 
-    int magic, guess, numGuesses, amountCorrect;
+    int magic, guess, totalGuesses;
+    char numGuess;
 
     unsigned int seed;
     seed = time(NULL);
     srand(seed);
 
+    cout << "Would you like to guess at a magic number? (y/n) ";
+    cin >> numGuess;
 
 
+        while (numGuess == 'y') {
 
-    cout << "How many numbers would you like to guess at? ";
-    cin >> numGuesses;
-
-    for(int x = 0; x < numGuesses;x++) {
-        magic = rand()%20+1;
-        bool gotIt=false;
-        cout << "Guess magic number #"<<x+1<<":\n";
-
-       for(int i = 0; i < 5 && !gotIt; i++) {
+            magic = rand() % 20 + 1;
+            totalGuesses = 0;
 
 
-            cout<<"Enter your guess: ";
-            cin>>guess;
-            cout<<endl;
+            do {
 
-                for(;guess <=0 || guess >20;) {
-                    cout<< "Oops! That's not a valid number... Try again: ";
-                    cin>>guess;
+                cout << "Enter your guess: ";
+                cin >> guess;
+                cout << endl;
+
+                while (guess <= 0 || guess > 20) {
+                    cout << "Oops! That's not a valid number... Try again: ";
+                    cin >> guess;
                 }
 
 
-            if(guess==magic) {
-                cout<<"You got it right on guess #" <<i+1<<"\n\n";
-                gotIt=true;
-                amountCorrect++;
+                if (guess == magic) {
+                    cout << "You got it right!\n";
+                    totalGuesses++;
 
-            }else if(guess>magic) {
-                cout<<"You guessed too high\n\n";
+                } else if (guess > magic) {
+                    cout << "You guessed too high\n\n";
+                    totalGuesses++;
 
+                } else {
+                    cout << "You guessed too low\n\n";
+                    totalGuesses++;
+                }
 
+            } while (guess!=magic);
+
+            if(totalGuesses <= 3) {
+                cout << "\nExcellent! you got it right in " << totalGuesses << " guesses!\n\n";
+            } else if(totalGuesses > 3 && totalGuesses <= 5) {
+                cout << "\nGood! you got it right in " << totalGuesses << " guesses!\n\n";
+            } else if(totalGuesses > 5 && totalGuesses <= 7) {
+                cout << "\nAverage. you got it right in " << totalGuesses << " guesses!\n\n";
             } else {
-                cout<<"You guessed too low\n\n";
-
+                cout << "\nPoor :( you got it right in " << totalGuesses << " guesses!\n\n";
             }
+
+            cout << "Would you like to play again? (y/n) ";
+            cin >> numGuess;
+
 
         }
 
+        cout << "Thank you for playing!\n";
 
+
+
+        return 0;
     }
-    cout<<"\nYou got " << amountCorrect <<" right\n";
 
-
-    return 0;
-
-
-}
